03aee27ae35a168371cc133959bc037d5b83b417
Christopher B. Gonzalez <147759486+ChrisBGonz@users.noreply.github.com>
2023-10-19 11:48:54 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    
+    cout << "Enter an integer: "; // User prompt
+    cin >> a; //Value entered by user becomes a
+    
+    cout << "You entered " << a << endl;
+    
+    return 0;
+}
+// This shows you how the user can interact with program by having them define a
