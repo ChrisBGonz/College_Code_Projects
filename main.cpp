dbd1d0167eb7a2ccf6af8a197dfe23984e3715fc
Christopher B. Gonzalez <147759486+ChrisBGonz@users.noreply.github.com>
2023-10-19 11:49:44 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//This program allows user to do enter values for addition
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, sum;
+    
+    cout << "Enter an integer: ";
+    cin >> a;
+    
+    cout << "Enter another integer: ";
+    cin >> b;
+    
+    sum = a+b;
+    
+    cout << "The sum is: " << sum << endl;
+    
+    return 0;
+}
