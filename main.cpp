12ab533c66fdf92806054baa55ca73b513209b29
Christopher B. Gonzalez <147759486+ChrisBGonz@users.noreply.github.com>
2023-10-19 12:38:44 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+using namespace std;
+
+int x = 2;
+int main()
+{
+    int a = 4, b = 7;
+    
+    cout << x << endl;
+    cout << a << endl;
+    cout << b << endl;
+    
+    int x = 10;
+    
+    cout << x << endl;
+
+    return 0;
+}
