ba63c1692b7675320d0966deeb2691e794270ccb
Christopher B. Gonzalez <147759486+ChrisBGonz@users.noreply.github.com>
2023-10-19 11:57:50 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/* This program will obtain three integers from the user and calculate and output
+ their sum and difference.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    float sum, average;
+    
+    cout << "Enter three integers separated by spaces: ";
+    cin >> a >> b >> c;
+    
+    sum = a + b + c;
+    average = sum / 3;
+    
+    cout << "The sum is " << sum << endl;
+    cout << "The average is " << average << endl;
+    
+    return 0;
+}
