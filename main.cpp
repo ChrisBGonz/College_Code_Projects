00c3b95e2cfe6216429717e154f6e0a8fa420f7c
Christopher B. Gonzalez <147759486+ChrisBGonz@users.noreply.github.com>
2023-10-19 11:47:02 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    
+    cout << "Value of var. a after the declaration: " << a << endl;
+    
+    a = 4;
+    
+    cout << a << endl;
+    
+    cout << "Value of var. a after its initialization: " << a << endl;
+    
+    return 0;
+}
+// The point of this is to show you the importance of initializing a variable before declaring it.
