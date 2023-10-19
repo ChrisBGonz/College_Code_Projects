fad98f40c4e746de16a19cf04db72a542de0da0c
Christopher B. Gonzalez <147759486+ChrisBGonz@users.noreply.github.com>
2023-10-19 12:26:56 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/*
+ This program will use a for loop and obtain five integers from the user and determine if each integer is a multiple of 5.
+ */
+
+# include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i, a;
+    
+    cout << "Enter five integers separated by spaces: ";
+    
+    for (i = 1; i <= 5; i++)
+    {
+        cin >> a;
+        
+        if (a % 5 == 0)
+            cout << a << " is a multiple of 5" << endl << endl;
+        
+        else
+            cout << a << " is not a multiple of 5" << endl << endl;
+    }
+    
+    return 0;
+}
